320a4ce5eaeb4bbb253a277f74e8cad938142621
matheus258 <matheus_marques258@outlook.com>
2023-01-22 22:23:29 -0300

    Comentando codigo

--- Projects JavaScript/Projeto_QrCode/index.js
+++ Projects JavaScript/Projeto_QrCode/index.js
@@ -1,3 +1,5 @@
+// Codigo Js para o Projeto
+
 const container = document.querySelector('.container');
 qrInput = container.querySelector('.form input');
 generateBtn = container.querySelector('.form button');
@@ -6,20 +8,21 @@ qrImg = container.querySelector('.qrcode .img');
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
     if(!qrValue){
-        alert('Insira uma url valida!')
+        alert('Insira uma url valida!');
         return;
     }
     generateBtn.innerText = "Gerando Qr-code...";
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`
-
+    // Api QrCode
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    // Adicionando classe para mostrar QrCode.
     qrImg.addEventListener('load', () => {
         generateBtn.innerText = 'Gerar Qr Code';
         container.classList.add('ativado');
-        container.classList.remove('desativado')
+        container.classList.remove('desativado');
     })
 
 });
-
+// Quando imput não tiver valor, esconder imagem e apagar classe.
 qrInput.addEventListener('keyup', () => {
     if(!qrInput.value){
         container.classList.remove('ativado');
