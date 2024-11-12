e803df18135e039ef2ce9db5516e05274c0260b1
matheus258 <matheusmarquesaraujo53@gmail.com>
2024-11-12 12:53:49 -0300

    Iniciando projeto Spring delivery-api

---
+++ Projects Java/projectfood-api/src/test/java/com/gourmet/gourmetfood/ProjectfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourmet.gourmetfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
