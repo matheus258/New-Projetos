4ed88af5142f581ec71860fc95d21871761cf620
matheus258 <matheusmarquesaraujo53@gmail.com>
2024-11-17 20:22:26 -0300

    Enviando e recebendo parametros POST json

---
+++ Projects Java/FamaciaApi/src/main/java/com/remedios/marques/FamaciaApi/FamaciaApiApplication.java
@@ -0,0 +1,13 @@
+package com.remedios.marques.FamaciaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamaciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamaciaApiApplication.class, args);
+	}
+
+}
---
+++ Projects Java/FamaciaApi/src/test/java/com/remedios/marques/FamaciaApi/FamaciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remedios.marques.FamaciaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamaciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
