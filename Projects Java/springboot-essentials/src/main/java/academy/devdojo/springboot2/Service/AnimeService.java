0ad457edc6d171e7226c0be0c59a1ba98f057a01
matheus258 <matheusmarquesaraujo53@gmail.com>
2024-12-03 17:48:00 -0300

    config err logs

--- Projects Java/springboot-essentials/src/main/java/academy/devdojo/springboot2/Service/AnimeService.java
+++ Projects Java/springboot-essentials/src/main/java/academy/devdojo/springboot2/Service/AnimeService.java
@@ -2,13 +2,24 @@ package academy.devdojo.springboot2.Service;
 
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService implements AnimeRepository {
+    private List<Anime> animes = List.of(new Anime(1L, "One Piece"), new Anime(2L, "Berserk"), new Anime(3L, "Naruto"));
+
     public List<Anime> ListAll(){
-        return List.of(new Anime(1L, "One Piece"), new Anime(2L, "Berserk"), new Anime(3L, "Naruto"));
+        return animes;
+    }
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found!"));
     }
 }
--- Projects Java/springboot-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ Projects Java/springboot-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -7,7 +7,11 @@ import academy.devdojo.springboot2.util.DateUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +27,13 @@ public class AnimeController {
     private DateUtil dateUtil;
 
     @GetMapping("list")
-    public List<Anime> List(){
+    public ResponseEntity<List<Anime>> List(){
         log.info(dateUtil.formatLocalDateTimeToDataStyle(LocalDateTime.now()));
-        return animeService.ListAll();
+        return new ResponseEntity<>(animeService.ListAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
